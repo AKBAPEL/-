ab022ae16569772bab17bc4a9cfe0621a337369e
AKBAPEL <beldom2005@mail.ru>
2021-02-01 18:08:23 +0400

    Исправил ошибку в коде

--- sorting.py
+++ sorting.py
@@ -31,11 +31,11 @@ def insert_sort(unsortred_data: list) -> list:
     for i in range(1, len(data)):
         current = data[i]
         j = i - 1
-        while data[i] < data[j]  and j>= 0:
-            data[i], data[j] = data[j], data[i]
+        while data[j] > current  and j>= 0:
+            data[j + 1] = data[j]
             j = j - 1
         
-        data[j] = current
+        data[j+1] = current
 
     return data
 
